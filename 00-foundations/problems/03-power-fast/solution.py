548581d045a4b993e13e88bb6a58059cc92bde5f
Lourdhu Raju <b.lourdhuraju1234@gmail.com>
2026-05-18 13:25:09 +0530

    03-power-fast

--- 00-foundations/problems/03-power-fast/solution.py
+++ 00-foundations/problems/03-power-fast/solution.py
@@ -1,17 +1,33 @@
 """Starter."""
 
+from unittest import result
+
 
 def power_linear(a: int, n: int) -> int:
+    result = 1
     if n == 0:
         return 1
     else:
         for i in range(1, n + 1):
-            a *= a
-    return a
+            result *= a
+    return result
     # TODO: multiply ``a`` by itself ``n`` times. Handle n == 0.
     raise NotImplementedError
 
 
 def power_fast(a: int, n: int) -> int:
+    if n == 0:
+        return 1
+    else:
+        if n % 2 == 0:
+            half = power_fast(a, (n // 2))
+            result = half * half
+        else:
+            odd = power_fast(a, n - 1)
+            result = a * odd
+    return result
+
+    return result
+
     # TODO: exponentiation by squaring — Θ(log n) multiplications.
     raise NotImplementedError
